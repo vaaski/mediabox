80604b21aecf6210e3574486b28c5f953f37a728
vaaski <admin@vaa.ski>
2023-03-18 15:56:02 +0100

    feat: blurry window

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from "vue";
-import "./styles.css";
-import App from "./App.vue";
+import { createApp } from "vue"
+import "./styles.css"
+import App from "./App.vue"
+import "@acab/reset.css"
 
-createApp(App).mount("#app");
+createApp(App).mount("#app")
