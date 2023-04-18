b5e81ed3397c2b9fa96fc5305361c75069e27539
vaaski <admin@vaa.ski>
2023-04-18 23:00:07 +0200

    feat: better logging view

--- src/routes/LogView.vue
+++ src/routes/LogView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { nextTick, ref, watch } from "vue"
-import { accumulatedLog } from "../logging"
+import { makeLogger, accumulatedLog } from "../logging"
+const log = makeLogger("log-view")
 
 const logElement = ref<HTMLPreElement>()
 watch(accumulatedLog, async () => {
   if (!logElement.value) return
 
   const isAtBottom =
-    logElement.value.scrollTop ===
+    logElement.value.scrollTop + 25 >=
     logElement.value.scrollHeight - logElement.value.clientHeight
 
   if (!isAtBottom) return
@@ -16,11 +17,18 @@ watch(accumulatedLog, async () => {
   logElement.value.scrollTop =
     logElement.value.scrollHeight - logElement.value.clientHeight
 })
+
+const logSomething = () => log(Math.random().toString(36))
+const reload = () => window.location.reload()
 </script>
 
 <template>
   <div class="wrap">
-    <pre ref="logElement">{{ accumulatedLog }}</pre>
+    <pre ref="logElement">{{ accumulatedLog || "logs will appear here" }}</pre>
+    <div class="controls">
+      <button @click="logSomething">log something</button>
+      <button @click="reload">reload</button>
+    </div>
   </div>
 </template>
 
@@ -30,6 +38,7 @@ watch(accumulatedLog, async () => {
   height: 100%;
   width: 100%;
   display: flex;
+  flex-direction: column;
 }
 
 pre {
